9f58685feb6188675d97a2e3a8ba6d5da55ee8b1
Manar Mansour <manar.mansour@aucegypt.edu>
2022-02-08 01:42:20 +0200

    first commit

---
+++ udagram-api/src/sequelize.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+
+/*export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.dbPort,
+
+  dialect: "postgres",
+  storage: ":memory:",
+});*/
+
+export const sequelize = new Sequelize(
+  `${config.dialect}://${config.username}:${config.password}@${config.host}:${config.dbPort}/${config.database}`
+);
